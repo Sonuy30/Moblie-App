15f3f29c3c97e34d7b3ac23c931917ce3761c58b
Sonuy30 <studymaewq@gmail.com>
2026-08-04 09:29:01 +0530

    Fix EAS Gradle build settings and build properties for DailyNest shop app

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ module.exports = function (api) {
       ['babel-preset-expo', { unstable_transformImportMeta: true }],
     ],
     plugins: [
-      'babel-plugin-transform-import-meta',
       [
         'module-resolver',
         {
